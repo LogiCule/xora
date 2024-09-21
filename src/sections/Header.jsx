ce62c6a9524be565ac33d45badcf7f19dc9bd8b8
Rahul Dubey <rahuldubey7398@gmail.com>
2024-09-21 15:47:44 +0530

    features section added

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -4,7 +4,12 @@ import { Link as LinkScroll } from "react-scroll";
 import clsx from "clsx";
 
 const NavLink = ({ title }) => (
-  <LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+  <LinkScroll
+    to={title}
+    className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5"
+    spy
+    smooth
+  >
     {title}
   </LinkScroll>
 );
@@ -24,7 +29,6 @@ const Header = () => {
             isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
           )}
         >
-          {/* className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0" */}
           <div className="max-lg:realtive max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
